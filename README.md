e511f5a4f16457fbb091ba3d29ede86753982290
zhenfei <zhenfei.zhang@hotmail.com>
2022-04-01 10:09:36 -0400

    support falcon-1024 parameter set

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-This is a simple rust wrapper of falcon signature scheme, submitted to NIST PQC competition.
-Currently it only support Falcon-512 parameter set.
+Falcon-rust
+------
+
+This is a rust wrapper of falcon signature scheme, submitted to NIST PQC competition.
+It supports both falcon-512 and falcon-1024 parameter sets. The default is set to falcon-1024.
+
+
+To build for falcon-1024
+```
+    cargo build [--release]
+```
+
+
+To build for falcon-512
+```
+    cargo build [--release] --no-default-features --features=falcon-512
+```
--- src/param.rs
+++ src/param.rs
@@ -1,13 +1,51 @@
-pub const MODULUS: u16 = 12289;
+pub const MODULUS: u32 = 12289;
 
-pub const PK_LEN: usize = 897;
-pub const SK_LEN: usize = 1281;
-pub const SIG_LEN: usize = 666;
+// (q-1)/2
+pub const MODULUS_MINUS_1_OVER_TWO: i16 = 6144;
 
-pub const KEYGEN_BUF_LEN: usize = 15879;
-pub const SIGN_BUF_LEN: usize = 39943;
-pub const MAKE_PK_BUF_LEN: usize = 3073;
-pub const VERIFY_BUF_LEN: usize = 4097;
+/// largest multiple of q that is smaller than 2^16
+pub const MODULUS_THRESHOLD: u16 = 61445;
 
-pub const SIG_COEFF_BIT_LEN: usize = 12;
-pub const SIG_L2_BOUND: u32 = 34034726;
+#[cfg(feature = "falcon-1024")]
+pub use param1024::*;
+
+#[cfg(feature = "falcon-512")]
+pub use param512::*;
+
+#[cfg(feature = "falcon-512")]
+mod param512 {
+    pub const LOG_N: usize = 9;
+    pub const N: usize = 512;
+    pub const ONE_OVER_N: u32 = 12265;
+
+    pub const PK_LEN: usize = 897;
+    pub const SK_LEN: usize = 1281;
+    pub const SIG_LEN: usize = 666;
+
+    pub const KEYGEN_BUF_LEN: usize = 15879;
+    pub const SIGN_BUF_LEN: usize = 39943;
+    pub const MAKE_PK_BUF_LEN: usize = 3073;
+    pub const VERIFY_BUF_LEN: usize = 4097;
+
+    // pub const SIG_COEFF_BIT_LEN: usize = 12;
+    pub const SIG_L2_BOUND: u32 = 34034726;
+}
+
+#[cfg(feature = "falcon-1024")]
+mod param1024 {
+    pub const LOG_N: usize = 10;
+    pub const N: usize = 1024;
+    pub const ONE_OVER_N: u32 = 12277;
+
+    pub const PK_LEN: usize = 1793;
+    pub const SK_LEN: usize = 2305;
+    pub const SIG_LEN: usize = 1280;
+
+    pub const KEYGEN_BUF_LEN: usize = 31751;
+    pub const SIGN_BUF_LEN: usize = 79879;
+    pub const MAKE_PK_BUF_LEN: usize = 6145;
+    pub const VERIFY_BUF_LEN: usize = 8193;
+
+    // pub const SIG_COEFF_BIT_LEN: usize = 12;
+    pub const SIG_L2_BOUND: u32 = 70265242;
+}
